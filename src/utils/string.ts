5acfdc89d495aec223207b4bbdaa9efdb5482b17
Mustafa Keskin <1378380+mkeskin@users.noreply.github.com>
2023-01-10 14:10:56 +0300

    feat: A plurize method for strings has been added.

---
+++ src/utils/string.ts
@@ -0,0 +1,18 @@
+/**
+ * Pluralizes a word in English.
+ *
+ * @param {string} word - The word to pluralize
+ * @return {string} The plural form of the word
+ */
+export const pluralize = (word: string): string => {
+  // If the word ends in "s", "sh", "ch", "x", or "z", add "es" to the end of the word
+  if (/[s|sh|ch|x|z]$/.test(word)) {
+    return `${word}es`
+  }
+  // If the word ends in "y" and the letter before the "y" is a consonant, remove the "y" and add "ies"
+  if (/[^aeiou]y$/.test(word)) {
+    return `${word.slice(0, -1)}ies`
+  }
+  // In all other cases, add "s" to the end of the word
+  return `${word}s`
+}
