903fc175758000f6944923a4175c6db465d6f72d
Mustafa Keskin <1378380+mkeskin@users.noreply.github.com>
2023-01-10 15:04:06 +0300

    feat: The cache has been added to fetching.

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,8 +1,21 @@
 import wretch from 'wretch'
+import { throttlingCache } from 'wretch/middlewares'
 import qs from 'wretch/addons/queryString'
 
 const w = wretch()
-  .resolve((r) => r.json())
+  .middlewares([
+    throttlingCache({
+      /* Options - defaults below */
+      throttle: 1000,
+      skip: (url, opts) => opts.skipCache || opts.method !== 'GET',
+      key: (url, opts) => opts.method + '@' + url,
+      clear: (url, opts) => false,
+      invalidate: (url, opts) => null,
+      condition: (response) => response.ok,
+      flagResponseOnCacheHit: '__cached',
+    }),
+  ])
   .addon(qs)
+  .resolve((r) => r.json())
 
 export const bookService = w.url(process.env.NEXT_PUBLIC_OPENLIBRARY_BASEURL)
